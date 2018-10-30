90ad72464f01bc73de31b434afff742d4671f6bc
Valeria Kozlova <kozlova.valeriia@gmail.com>
2018-10-30 21:54:16 +0000

    Setting up pages in views

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var PORT = 8080;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -9,10 +11,7 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,6 +24,32 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>ßWorld</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  let longUrl = urlDatabase[req.params.id];
+  let templateVars = { shortURL: req.params.id, longURL: longUrl};
+  res.render("urls_new", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let longUrl = urlDatabase[req.params.id];
+  let templateVars = { shortURL: req.params.id, longURL: longUrl};
+  res.render("urls_show", templateVars);
+});
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
